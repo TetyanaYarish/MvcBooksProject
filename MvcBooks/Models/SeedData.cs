9284126c17f2294788457cc9e0407c465aab00f1
avam kuda <avamkuda@gmail.com>
2020-09-29 16:17:30 +1000

    First version

---
+++ MvcBooks/Models/SeedData.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcBooks.Models;
+using System;
+using System.Linq;
+
+namespace MvcBooks.Data
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcBooksContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcBooksContext>>()))
+            {
+                // Look for any movies.
+                if (context.Book.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Book.AddRange(
+                    new Book
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Rating = "R",
+                        Price = 7.99M
+                    },
+
+                    new Book
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Rating = "R",
+                        Price = 8.99M
+                    },
+
+                    new Book
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Rating = "B",
+                        Price = 9.99M
+                    },
+
+                    new Book
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Rating = "A",
+                        Price = 3.99M
+                    },
+                    new  Book
+                    {
+                        Title = "Hohoho",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Rating = "A",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
